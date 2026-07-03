7ede93702f1f5143c465d95b756d90619062078a
Dmitriy Girak <rodnayasem@gmail.com>
2026-07-03 21:50:21 +0300

    Сделал списки инициализации

--- TestCpp/Adventurer.h
+++ TestCpp/Adventurer.h
@@ -11,18 +11,20 @@ class Adventurer {
   std::string rank;
 
  public:
-  Adventurer() {
-    id = 0;
-    name = "";
-    surname = "";
-    role = "";
-    level = 1;
-    reputation = 0;
-    rank = "Новичок";
-  }
+  Adventurer()
+      : id(0),
+        name(""),
+        surname(""),
+        role(""),
+        level(1),
+        reputation(0),
+        rank("Новичок") {}
   Adventurer(int id, std::string name, std::string surname, std::string role,
-             int level, int reputation) {
-    this->id = id;
-    this->name = name;
-  }
+             int level, int reputation)
+      : id(id),
+        name(name),
+        surname(surname),
+        role(role),
+        level(level),
+        reputation(reputation) {}
 };
